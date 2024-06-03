de558a03ce63715c58e44fda75b99eeba8079593
h3x0c4t <118213553+h3x0c4t@users.noreply.github.com>
2024-06-03 03:56:48 +0300

    init

---
+++ patch/include.h
@@ -0,0 +1,26 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "decorationhints.h"
+#include "focusfollowmouse.h"
+#include "pertag.h"
+#include "sizehints_ruled.h"
+#include "swallow.h"
+#include "warp.h"
+#include "zoomswap.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
